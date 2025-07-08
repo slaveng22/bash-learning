61235a2cb87832e2606986bd5427e23854c6bbbd
slaveng22 <117160808+slaveng22@users.noreply.github.com>
2025-07-08 19:03:56 +0200

    created log-extractor, added some things to system-monitor

--- log-extractor.sh
+++ log-extractor.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Define error patterns (case-insensitive)
+PATTERNS="error|fail|crit|alert|emerg|panic"
+
+# Find readable log files (skip compressed ones)
+LOGFILES=$(find /var/log -type f \( -name "*.log" -o -name "*.out" -o -name "*.log.1" \) 2>/dev/null | grep -vE "\.gz$|\.xz$")
+
+for LOG in $LOGFILES; do
+  MATCHES=$(grep -iE "$PATTERNS" "$LOG" 2>/dev/null)
+  if [ -n "$MATCHES" ]; then
+    echo -e "\n===== $LOG ====="
+    echo "$MATCHES"
+  fi
+done
+
--- system-monitor.sh
+++ system-monitor.sh
@@ -4,7 +4,7 @@
 # Task 4: Create a menu-driven script that performs system health checks.
 # Script: system-monitor.sh
 # Description: A simple, real-time monitoring script for Linux
-# It displays Hardware info, Network info, Kernel ersion, Uptime, OS info, Inodes, SWAP, Running services, CPU, Memory and Disk Usage
+# It displays Hardware info, Network info, Kernel version, Uptime, OS info, Inodes, SWAP, Running services, CPU, Memory and Disk Usage
 
 set -euo pipefail
 
@@ -20,14 +20,57 @@ NC='\e[0m' # No Color
 #--- Functions ---
 
 # CPU info
+
 get_cpu_info() {
 
-    CPU=$(lscpu | awk -F: '/Model name/ {print $2}')
-    # Trim whitespace
-    CPU=$(echo $CPU | xargs)
-    echo -e "${GREEN}---CPU Model---${NC}: $CPU"
+  CPU=$(lscpu | awk -F: '$1 ~ /^Model name$/ {print $2; exit}' | xargs)
+  echo -e "${GREEN}---CPU Model---${NC}: $CPU"
+
+}
+
+get_ram_model() {
+
+  RAM=$(lshw -class memory | grep -i "product" | head -1 | awk -F: '{print $2}' | xargs)
+  echo -e "${GREEN}---RAM Model---${NC}: $RAM"
+
+}
+
+get_wirless_card() {
+
+  WIRELESS=$(lspci | grep -i 'wireless' | cut -d ':' -f3- | xargs)
+  echo -e "${GREEN}---Wirless Card Model---${NC}: $WIRELESS"
+
+}
+
+get_ethernet_card() {
+
+  ETHERNET=$(lspci | grep -i 'ethernet' | cut -d ':' -f3- | xargs)
+  echo -e "${GREEN}---Ethernet Card Model---${NC}: $ETHERNET"
+
+}
+
+get_os_info() {
+
+  OS=$(lsb_release -a | grep -i Description | awk -F: '{print $2}' | xargs)
+  echo -e "${GREEN}---OS info---${NC}: $OS"
+
+}
+
+get_uptime() {
+  echo -e "${GREEN}---System Uptime---${NC}: $(uptime -p)"
+}
+
+get_kernel_version() {
+  echo -e "${GREEN}---Kernel Version---${NC}: $(uname -r)"
 }
 
 get_cpu_info
-
+get_ethernet_card
+get_wirless_card
+get_ram_model
+echo ""
+get_os_info
+get_uptime
+get_kernel_version
 # Network info
+
