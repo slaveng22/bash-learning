9713d3dff69504d90459c1aef11735880ec5e272
slaveng22 <Slaven.Gugolj@outlook.com>
2025-11-13 08:42:56 +0100

    Finished panindrome, started log analyzer script

--- 30-days-practice/day-6/palindrome.sh
+++ 30-days-practice/day-6/palindrome.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# Check if word or sentance is a palidrome
+# Check if word or sentance is a palindrome
 
 INPUT_TEXT=$1
-INPUT_TEXT=$(echo "$INPUT_TEXT" | tr -d '[:space:]')
+INPUT_TEXT=$(echo "$INPUT_TEXT" | tr '[:upper:]' '[:lower:]' | tr -d '[:space:]')
 
-echo "$INPUT_TEXT"
+REVERSE_TEXT=$(echo "$INPUT_TEXT" | rev)
+
+if [[ "$INPUT_TEXT" == "$REVERSE_TEXT" ]]; then
+  echo "Text you entered is palindrome"
+else
+  echo "Text you entered isn't palindrome"
+fi
---
+++ 30-days-practice/day-7/log_analyze.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Takes a log file path as input.
+# Counts how many lines contain the word "error" (case-insensitive).
+# Prints the 5 most recent error lines (from the bottom of the file).
+# Handles missing or unreadable file errors cleanly.
