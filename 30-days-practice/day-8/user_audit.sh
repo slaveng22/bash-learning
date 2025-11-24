190eca94746d5ab2c4bfed7333290b973fa49ed8
slaveng22 <Slaven.Gugolj@outlook.com>
2025-11-24 13:08:22 +0100

    finished user audit script

--- 30-days-practice/day-8/user_audit.sh
+++ 30-days-practice/day-8/user_audit.sh
@@ -11,10 +11,37 @@
 #
 # If the user has 0 running processes, print Note: User is currently not running any processes.
 #
-# Example outputL:
+# Example output:
 #   User: alice
 #   Home directory: /home/alice
 #   Shell: /bin/bash
 #   Running processes: 3
 #   Last login:
 #   alice    pts/0    192.168.0.12   Fri Nov 15 10:43   still logged in
+
+USER=$1
+
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 user"
+  exit 1
+fi
+
+if ! getent passwd | grep "$USER" >/dev/null; then
+  echo "User "$USER" doesn't exist on this system"
+  exit 1
+fi
+
+HOME_DIRECTORY=$(getent passwd | grep "$USER" | awk -F: '{print $(NF-1)}')
+USER_SHELL=$(getent passwd | grep "$USER" | awk -F: '{print $NF}')
+PROCESS_NO=$(ps -U "$USER" | wc -l)
+LAST_LOGIN=$(who | awk '{print $4,$3}')
+
+echo "User: "$USER""
+echo "Home directory: "$HOME_DIRECTORY""
+echo "Shell: "$USER_SHELL""
+if [[ "$PROCESS_NO" -ge 1 ]]; then
+  echo "Running processes: "$PROCESS_NO""
+else
+  echo "Note: User is currently not running any processes."
+fi
+echo "Last login: "$LAST_LOGIN""
